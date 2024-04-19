68ca1f396b3eacee748264df5db149b1c7da5b56
guiMendel <guimendeln@gmail.com>
2024-04-19 11:39:11 -0300

    chore: email + sms auth

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { FC } from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import { Home } from './src/Home'
+import { client } from './src/client'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: FC = () => (
+  <>
+    <client.reactNative.WebView />
+    <StatusBar style="auto" />
+
+    <SafeAreaView style={styles.main}>
+      <Home />
+    </SafeAreaView>
+  </>
+)
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
+
+export default App
