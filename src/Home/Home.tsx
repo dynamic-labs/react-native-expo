b3c53083302947e39609b4ac47fcc248cd0172b5
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2024-05-17 15:58:56 -0300

    chore: update @dynamic-labs/* to version 2.1.0-alpha.19

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,9 +3,14 @@ import { FC } from "react";
 import { client } from "../client";
 import { LoginView } from "../LoginView";
 import { DisplayAuthenticatedUserView } from "../DisplayAuthenticatedUserView";
+import { Text } from "react-native";
 
 export const Home: FC = () => {
-  const { auth } = useReactiveClient(client);
+  const { auth, sdk } = useReactiveClient(client);
+
+  if (!sdk.loaded) {
+    return <Text>Loading...</Text>;
+  }
 
   if (auth.token) {
     return <DisplayAuthenticatedUserView />;
