5e8e463415709220417a890db7eb70601ed87f6f
guiMendel <guimendeln@gmail.com>
2024-05-29 16:46:12 -0300

    chore: add farcaster sign in button

--- src/client.ts
+++ src/client.ts
@@ -1,14 +1,18 @@
-import { createClient } from "@dynamic-labs/client";
-import { ReactNativeExtension } from "@dynamic-labs/react-native-extension";
+import { createClient } from '@dynamic-labs/client'
+import { ReactNativeExtension } from '@dynamic-labs/react-native-extension'
 
-const environmentId = process.env.EXPO_PUBLIC_ENVIRONMENT_ID as string;
+const environmentId = process.env.EXPO_PUBLIC_ENVIRONMENT_ID as string
 
 if (!environmentId) {
-  throw new Error("EXPO_PUBLIC_ENVIRONMENT_ID is required");
+  throw new Error('EXPO_PUBLIC_ENVIRONMENT_ID is required')
 }
+
+const apiBaseUrl =
+  process.env.EXPO_PUBLIC_API_BASE_URL || 'https://app.dynamicauth.com/api/v0'
 
 export const client = createClient({
   environmentId,
-  appLogoUrl: "https://demo.dynamic.xyz/favicon-32x32.png",
-  appName: "Dynamic Demo",
-}).extend(ReactNativeExtension());
+  appLogoUrl: 'https://demo.dynamic.xyz/favicon-32x32.png',
+  appName: 'Dynamic Demo',
+  apiBaseUrl,
+}).extend(ReactNativeExtension())
